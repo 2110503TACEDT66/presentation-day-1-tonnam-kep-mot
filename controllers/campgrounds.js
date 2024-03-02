3759c40654c6f5a14616a6c0863a96919a98d19d
MyChadum07 <156446728+MyChadum07@users.noreply.github.com>
2024-03-02 07:29:42 +0700

    Finish Basic Rest(Tonnam)

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,15 +1,50 @@
-exports.getCampgrounds=(req,res,next)=>{
-    res.status(200).json({success:true, msg:'Get all Campgrounds'});
+
+const Campground = require('../models/campground')
+exports.getCampgrounds=async(req,res,next)=>{
+    try{
+        const campgrounds=await Campground.find()
+        res.status(200).json({success:true, count: campgrounds.length, data:campgrounds});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 }
-exports.getCampground=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Show Campgrounds ${req.params.id}`});
+exports.getCampground=async(req,res,next)=>{
+    try{
+        const campground=await Campground.findById(req.params.id);
+        if(!campground)
+            return res.status(400).json({success:false});
+        res.status(200).json({success:true, data: campground});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 }
-exports.createCampground=(req,res,next)=>{
-    res.status(200).json({success:true, msg:'Create new Campgrounds'});
+exports.createCampground=async(req,res,next)=>{
+    const campground = await Campground.create(req.body);
+    res.status(201).json({
+        success: true,
+        data:campground
+    });
 }
-exports.updateCampground=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Update Campgrounds ${req.params.id}`});
+exports.updateCampground=async(req,res,next)=>{
+    try{
+        const campground=await Campground.findByIdAndUpdate(req.params.id, req.body,{
+            new:true,
+            runValidators:true
+        })
+        if(!campground)
+            return res.status(400).json({success:false});
+        res.status(200).json({success:true, data: campground});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 }
-exports.deleteCampground=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Delete Campgrounds ${req.params.id}`});
+exports.deleteCampground=async(req,res,next)=>{
+    try{
+        const campground=await Campground.findByIdAndDelete(req.params.id)
+        if(!campground)
+            return res.status(400).json({success:false});
+        res.status(200).json({success:true, data: {}});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 }
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 const campgrounds = require ('./routes/campgrounds');
 dotenv.config({path:'./config/config.env'});
+connectDB();
 const app = express();
+app.use(express.json());
 app.use('/api/v1/campgrounds',campgrounds);
 const PORT=process.env.PORT || 7777;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' Mode on port ',PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' Mode on port ',PORT));
+process.on('unhandleRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1));
+});
